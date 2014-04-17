2f1e8f94cfdb79496acab9cbc72aa221f7e1cd2f
Ting-Yuan Hsia <ly2314@gmail.com>
2014-04-17 20:38:51 +0800

    LinearLayoutGenerateByCode code updated.
    Updated UI layout tree:
    LinearLayout (Vertical)
    - EditText (To)
    - EditText (Subject)
    - EditText (Message)
    - LinearLayout (Horizontal)
    -- Button (Send)
    -- Button (Cancel)

--- LinearLayoutGenerationByCode/src/com/ly2314/linearlayoutgenerationbycode/MainActivity.java
+++ LinearLayoutGenerationByCode/src/com/ly2314/linearlayoutgenerationbycode/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -61,20 +65,45 @@ public class MainActivity extends ActionBarActivity {
                 Bundle savedInstanceState) {
             //View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             //return rootView;
-        	LinearLayout ll = new LinearLayout(getActivity());
+        	LinearLayout rootView = new LinearLayout(getActivity());
+        	rootView.setOrientation(LinearLayout.VERTICAL);
         	
         	EditText _to = new EditText(getActivity());
+        	_to.setHint("To");
         	
         	EditText _subject = new EditText(getActivity());
+        	_subject.setHint("Subject");
         	
         	EditText _message = new EditText(getActivity());
+        	_message.setHint("Message");
+        	_message.setGravity(Gravity.TOP);
         	
-        	ll.addView(_to);
-        	ll.addView(_subject);
-        	ll.addView(_message);
+        	LinearLayout _buttons = new LinearLayout(getActivity());
+        	_buttons.setOrientation(LinearLayout.HORIZONTAL);
         	
+        	Button _send = new Button(getActivity());
+        	_send.setText("Send");
         	
-        	return ll;
+        	Button _cancel = new Button(getActivity());
+        	_cancel.setText("Cancel");
+        	
+        	_buttons.addView(_send);
+        	_buttons.addView(_cancel);
+        	
+        	rootView.addView(_to);
+        	rootView.addView(_subject);
+        	rootView.addView(_message);
+        	rootView.addView(_buttons);
+        	
+        	LinearLayout.LayoutParams _message_layout = (LinearLayout.LayoutParams)_message.getLayoutParams();
+        	_message_layout.height = 0;
+        	_message_layout.weight = 1;
+        	
+        	LinearLayout.LayoutParams _buttons_layout = (LinearLayout.LayoutParams)_buttons.getLayoutParams();
+        	_buttons_layout.width = LayoutParams.WRAP_CONTENT;
+        	_buttons_layout.gravity = Gravity.RIGHT;
+        	
+        	return rootView;
         }
     }
 
